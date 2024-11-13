994dd18fe05adc58dc9ddc8d2f64600821ad0bbc
katrinsukovati <katrin.sukovati1@gmail.com>
2024-11-13 17:08:02 -0500

    added route to get list of all inventory items

--- routes/inventories.js
+++ routes/inventories.js
@@ -1,6 +1,36 @@
 import express from "express";
+import initKnex from "knex";
+import configuration from "../knexfile.js";
+
 const router = express.Router();
+const knex = initKnex(configuration);
 
-
+// Route to get list of all inventory items
+router.route("/").get(async (req, res) => {
+  try {
+    const inventories = await knex("instock.inventories")
+      // Join to get warehouse name from warehouse table
+      .join(
+        "instock.warehouses",
+        "inventories.warehouse_id",
+        "=",
+        "warehouses.id"
+      )
+      .select(
+        "inventories.id",
+        "warehouses.warehouse_name as warehouse_name",
+        "inventories.item_name",
+        "inventories.description",
+        "inventories.category",
+        "inventories.status",
+        "inventories.quantity"
+      );
+    res.json(inventories);
+  } catch {
+    res.status(500).json({
+      message: "Error getting inventories",
+    });
+  }
+});
 
 export default router;
