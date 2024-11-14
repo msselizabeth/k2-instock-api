e6836cdcc0848e97a39204bdb6eb597899d56a58
katrinsukovati <katrin.sukovati1@gmail.com>
2024-11-14 10:46:41 -0500

    get all inventories for specified warehouse ID

--- controllers/warehouses-controller.js
+++ controllers/warehouses-controller.js
@@ -52,4 +52,36 @@ const getWarehouseById = async (req, res) => {
   }
 };
 
-export { getAllWarehouses, getWarehouseById };
+// API to GET Inventories for a Given Warehouse
+// GET /api/warehouses/:id/inventories
+const getInventoriesFromWarehouse = async (req, res) => {
+  try {
+    const inventoryOfWarehouse = await knex("warehouses")
+      // Join the two tables to get the warehouse name
+      .join("inventories", "warehouses.id", "=", "inventories.warehouse_id")
+      .select(
+        "warehouses.id",
+        "inventories.item_name",
+        "inventories.category",
+        "inventories.status",
+        "inventories.quantity"
+      )
+      // Here we are getting the id of the item
+      .where("warehouses.id", req.params.id);
+
+    // Inventory not found
+    if (!inventoryOfWarehouse) {
+      return res.status(404).json({
+        message: `Inventory of the warehousew was not found with id: ${req.params.id}`,
+      });
+    }
+    //  Only return if the warehouse inventory has been found
+    res.json(inventoryOfWarehouse);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error getting inventories for a given warehouse." });
+  }
+};
+
+export { getAllWarehouses, getWarehouseById, getInventoriesFromWarehouse };
--- routes/warehouses.js
+++ routes/warehouses.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   getAllWarehouses,
   getWarehouseById,
+  getInventoriesFromWarehouse,
 } from "../controllers/warehouses-controller.js";
 
 const router = express.Router();
@@ -9,5 +10,8 @@ const router = express.Router();
 router.get("/", getAllWarehouses);
 
 router.get("/:id", getWarehouseById);
+
+// Route to get inventories for a given warehouse
+router.get("/:id/inventories", getInventoriesFromWarehouse);
 
 export default router;
